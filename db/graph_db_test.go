d430454e1a7970577ae1d97cd14b3e95ddc084ae
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-12-08 23:43:05 +0530

    TestGetNodeByID Test Case

--- db/graph_db_test.go
+++ db/graph_db_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/sraynitjsr/model"
 )
 
-// MockNeo4jDriver is a mock implementation of the neo4j.Driver interface
 type MockNeo4jDriver struct {
 	mock.Mock
 }
@@ -24,28 +23,49 @@ func (m *MockNeo4jDriver) Close() error {
 	return args.Error(0)
 }
 
+type MockNeo4jSession struct {
+	mock.Mock
+}
+
+func (m *MockNeo4jSession) Run(cypher string, params map[string]interface{}) (neo4j.Result, error) {
+	args := m.Called(cypher, params)
+	return args.Get(0).(neo4j.Result), args.Error(1)
+}
+
+func (m *MockNeo4jSession) Close() error {
+	args := m.Called()
+	return args.Error(0)
+}
+
 func TestGetNodes(t *testing.T) {
-	// Create a mock Neo4j driver
 	mockDriver := new(MockNeo4jDriver)
-
-	// Create a GraphDatabase with the mock driver
 	graphDB := &GraphDatabase{dbDriver: mockDriver}
-
-	// Create a mock session
 	mockSession := new(MockNeo4jSession)
 	mockDriver.On("NewSession", mock.Anything).Return(mockSession)
-
-	// Set up expectations for the session method calls
-	mockSession.On("Run", mock.Anything, mock.Anything).Return(new(MockNeo4jResult), nil)
+	mockSession.On("Run", "MATCH (n:Node) RETURN n", nil).
+		Return(new(MockNeo4jResult), nil)
 	mockSession.On("Close").Return(nil)
 
-	// Call the method to be tested
 	_, err := graphDB.GetNodes()
 
-	// Assert that expectations were met
 	mockDriver.AssertExpectations(t)
 	mockSession.AssertExpectations(t)
+	assert.NoError(t, err)
+}
 
-	// Assert the result of the method
+func TestGetNodeByID(t *testing.T) {
+	mockDriver := new(MockNeo4jDriver)
+	graphDB := &GraphDatabase{dbDriver: mockDriver}
+	mockSession := new(MockNeo4jSession)
+	mockDriver.On("NewSession", mock.Anything).Return(mockSession)
+	expectedID := 123
+	mockSession.On("Run", "MATCH (n:Node) WHERE ID(n) = $id RETURN n", map[string]interface{}{"id": expectedID}).
+		Return(new(MockNeo4jResult), nil)
+	mockSession.On("Close").Return(nil)
+
+	_, err := graphDB.GetNodeByID(expectedID)
+
+	mockDriver.AssertExpectations(t)
+	mockSession.AssertExpectations(t)
 	assert.NoError(t, err)
 }
